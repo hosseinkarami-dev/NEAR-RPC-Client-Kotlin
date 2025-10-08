6c46b17c17ab5871c4b3e1dacd2bdc288f5f3435
Hossein Karami <49790581+hosseinkaramii@users.noreply.github.com>
2025-10-08 20:30:36 +0330

    fixed some bugs

--- app/src/main/kotlin/io/github/hosseinkarami_dev/near/rpc/client_app/MainActivity.kt
+++ app/src/main/kotlin/io/github/hosseinkarami_dev/near/rpc/client_app/MainActivity.kt
@@ -2,17 +2,10 @@ package io.github.hosseinkarami_dev.near.rpc.client_app
 
 import android.os.Bundle
 import androidx.core.app.ComponentActivity
-import androidx.lifecycle.lifecycleScope
-import io.github.hosseinkarami_dev.near.rpc.client.NearClient
-import io.github.hosseinkarami_dev.near.rpc.client.RpcResponse
-import io.github.hosseinkarami_dev.near.rpc.models.RpcTransactionStatusRequest
-import io.github.hosseinkarami_dev.near.rpc.models.SignedTransaction
-import io.github.hosseinkarami_dev.near.rpc.models.TxExecutionStatus
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,31 +17,31 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        val nearClient = NearClient(
-            httpClient = httpClient,
-            baseUrl = "https://rpc.mainnet.near.org" // or testnet: https://rpc.testnet.near.org
-        )
-
-        lifecycleScope.launch {
-            val response = nearClient.tx(
-                RpcTransactionStatusRequest.SignedTxBase64(
-                signedTxBase64 = SignedTransaction("FtzDPgG7BnX3g6WV7w951TZ1UErFahfp6NwQKiTE9dMp"),
-                waitUntil = TxExecutionStatus.Final
-            ))
- //           val response = nearClient.status()
-//            val response = nearClient.gasPrice(RpcGasPriceRequest(blockId = null))
-
-            when (response) {
-                is RpcResponse.Failure -> {
-                    println("Error: ${response.error}")
-                }
-
-                is RpcResponse.Success -> {
-                    val result = response.result
-                    println("Result: $result")
-
-                }
-            }
-        }
+//        val nearClient = NearClient(
+//            httpClient = httpClient,
+//            baseUrl = "https://rpc.mainnet.near.org" // or testnet: https://rpc.testnet.near.org
+//        )
+//
+//        lifecycleScope.launch {
+//            val response = nearClient.tx(
+//                RpcTransactionStatusRequest.SignedTxBase64(
+//                signedTxBase64 = SignedTransaction("FtzDPgG7BnX3g6WV7w951TZ1UErFahfp6NwQKiTE9dMp"),
+//                waitUntil = TxExecutionStatus.Final
+//            ))
+// //           val response = nearClient.status()
+////            val response = nearClient.gasPrice(RpcGasPriceRequest(blockId = null))
+//
+//            when (response) {
+//                is RpcResponse.Failure -> {
+//                    println("Error: ${response.error}")
+//                }
+//
+//                is RpcResponse.Success -> {
+//                    val result = response.result
+//                    println("Result: $result")
+//
+//                }
+//            }
+//        }
     }
 }
--- client/src/test/kotlin/io/github/hosseinkarami_dev/near/rpc/client/NearClientTest.kt
+++ client/src/test/kotlin/io/github/hosseinkarami_dev/near/rpc/client/NearClientTest.kt
@@ -1,13 +1,5 @@
 package io.github.hosseinkarami_dev.near.rpc.client
 
-import io.github.hosseinkarami_dev.near.rpc.models.AccountId
-import io.github.hosseinkarami_dev.near.rpc.models.BlockId
-import io.github.hosseinkarami_dev.near.rpc.models.CryptoHash
-import io.github.hosseinkarami_dev.near.rpc.models.RpcBlockRequest
-import io.github.hosseinkarami_dev.near.rpc.models.RpcMaintenanceWindowsRequest
-import io.github.hosseinkarami_dev.near.rpc.models.RpcStateChangesInBlockByTypeRequest
-import io.github.hosseinkarami_dev.near.rpc.models.RpcTransactionStatusRequest
-import io.github.hosseinkarami_dev.near.rpc.models.SignedTransaction
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -22,89 +14,13 @@ class NearClientTest {
             json()
         }
     }
-
-    private val nearClient = NearClient(
-        httpClient = httpClient,
-        baseUrl = "https://rpc.mainnet.near.org"
-    )
+//
+//    private val nearClient = NearClient(
+//        httpClient = httpClient,
+//        baseUrl = "https://rpc.mainnet.near.org"
+//    )
 
     @Test
     fun testTransactionStatus() = runTest {
-//        val response = nearClient.query(
-//            RpcQueryRequest.ViewAccessKeyListByFinality(
-//                finality = Finality.FINAL,
-//                accountId = AccountId("neardome2340.near"),
-//             //   publicKey = PublicKey("ed25519:EddTahJwZpJjYPPmat7DBm1m2vdrFBzVv7e3T4hzkENd"),
-//                requestType = RpcQueryRequest.ViewAccessKeyListByFinality.RequestType.VIEW_ACCESS_KEY_LIST
-//            )
-//        )
-
-        val response = nearClient.experimentalTxStatus(
-            RpcTransactionStatusRequest.SignedTxBase64(
-                SignedTransaction("Gz2YwFsq12yZDAVmA8M53DZM4ih56z6Ybhwd9QNa8A94")
-
-            )
-//            RpcMaintenanceWindowsRequest(
-//                AccountId("neardome2340.near")
-//            )
-        )
-
-        when (response) {
-            is RpcResponse.Failure -> {
-                println(response.error)
-                println("hiiiiiiiii")
-            }
-
-            is RpcResponse.Success -> {
-                println(response.result)
-//               val accessKeyList = response.getResultOrNull<RpcQueryResponse.AccountView>()
-//               println(accessKeyList)
-            }
-        }
-
-//        val response = nearClient.experimentalProtocolConfig(
-//            RpcProtocolConfigRequest.Finality(finality = Finality.FINAL)
-//        )
-
-//        when (response) {
-//            is RpcResponse.Failure -> {
-//                println(response.error)
-//            }
-//
-//            is RpcResponse.Success<*> -> {
-//                println(response)
-//
-//            }
-//        }
-
-
-        //val response = nearClient.status()
-//        val response = nearClient.changes(
-//            RpcStateChangesInBlockByTypeRequest.DataChangesByFinality(
-//                changesType = RpcStateChangesInBlockByTypeRequest.DataChangesByFinality.ChangesType.DATA_CHANGES,
-//                finality = Finality.FINAL,
-//                accountIds = listOf(AccountId("contract.rpc-examples.testnet")),
-//                keyPrefixBase64 = StoreKey("c29tZV9wcmVmaXg="),
-//            )
-//        )
-//        val response = nearClient.tx(
-//            RpcTransactionStatusRequest.SignedTxBase64(
-//                signedTxBase64 = SignedTransaction("FtzDPgG7BnX3g6WV7w951TZ1UErFahfp6NwQKiTE9dMp"),
-//                waitUntil = null,
-//            //    waitUntil = TxExecutionStatus.Final
-//            )
-//        )
-//
-//        when (response) {
-//            is RpcResponse.Failure -> {
-//                println("Error: ${response.error.code} ${response.error.message}")
-//                assert(false)
-//            }
-//
-//            is RpcResponse.Success -> {
-//                println("Result: ${response.result}")
-//                assert(true)
-//            }
-//        }
     }
 }
