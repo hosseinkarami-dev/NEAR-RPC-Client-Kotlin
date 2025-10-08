e6a8f8062bd5f9fd3f2ecdc7b07f20df5fbb4a3a
Hossein Karami <49790581+hosseinkaramii@users.noreply.github.com>
2025-10-08 20:25:58 +0330

    fixed some bugs

--- client/src/main/kotlin/io/github/hosseinkarami_dev/near/rpc/client/Utils.kt
+++ client/src/main/kotlin/io/github/hosseinkarami_dev/near/rpc/client/Utils.kt
@@ -1,18 +1,18 @@
 package io.github.hosseinkarami_dev.near.rpc.client
 
-import io.github.hosseinkarami_dev.near.rpc.models.RpcError
-import kotlinx.serialization.json.JsonPrimitive
-
-object Utils {
-    inline fun <reified T> RpcResponse<*>.getResultOrNull(): T? = (this as? RpcResponse.Success<*>)?.result as T?
-}
-
-fun localToRpcError(e: Throwable, localCode: Long = -1002L): RpcError.InternalError {
-    return RpcError.InternalError(
-        cause = JsonPrimitive(e::class.simpleName ?: "LocalException"),
-        name = RpcError.InternalError.Name.INTERNAL_ERROR,
-        code = localCode,
-        `data` = null,
-        message = e.message ?: "Local error: ${e::class.simpleName}"
-    )
-}
+//import io.github.hosseinkarami_dev.near.rpc.models.RpcError
+//import kotlinx.serialization.json.JsonPrimitive
+//
+//object Utils {
+//    inline fun <reified T> RpcResponse<*>.getResultOrNull(): T? = (this as? RpcResponse.Success<*>)?.result as T?
+//}
+//
+//fun localToRpcError(e: Throwable, localCode: Long = -1002L): RpcError.InternalError {
+//    return RpcError.InternalError(
+//        cause = JsonPrimitive(e::class.simpleName ?: "LocalException"),
+//        name = RpcError.InternalError.Name.INTERNAL_ERROR,
+//        code = localCode,
+//        `data` = null,
+//        message = e.message ?: "Local error: ${e::class.simpleName}"
+//    )
+//}
