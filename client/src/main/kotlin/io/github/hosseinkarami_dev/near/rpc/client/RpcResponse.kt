27cc1276e968ad1f887486cd54954c96a6a17d20
Hossein Karami <49790581+hosseinkaramii@users.noreply.github.com>
2025-10-08 21:40:14 +0330

    fixed some bugs

--- client/src/main/kotlin/io/github/hosseinkarami_dev/near/rpc/client/RpcResponse.kt
+++ client/src/main/kotlin/io/github/hosseinkarami_dev/near/rpc/client/RpcResponse.kt
@@ -1,8 +1,8 @@
 package io.github.hosseinkarami_dev.near.rpc.client
 
-//import io.github.hosseinkarami_dev.near.rpc.models.RpcError
-//
-//sealed class RpcResponse<out T> {
-//    data class Success<T>(val result: T): RpcResponse<T>()
-//    data class Failure(val error: RpcError): RpcResponse<Nothing>()
-//}
+import io.github.hosseinkarami_dev.near.rpc.models.RpcError
+
+sealed class RpcResponse<out T> {
+    data class Success<T>(val result: T): RpcResponse<T>()
+    data class Failure(val error: RpcError): RpcResponse<Nothing>()
+}
