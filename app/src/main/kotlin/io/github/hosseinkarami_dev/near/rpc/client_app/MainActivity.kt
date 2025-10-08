5dedc18be8f4934bc97b4a57cbb7b29ce5cb717a
Hossein Karami <49790581+hosseinkaramii@users.noreply.github.com>
2025-10-08 22:27:52 +0330

    fixed some bugs

--- app/src/main/kotlin/io/github/hosseinkarami_dev/near/rpc/client_app/MainActivity.kt
+++ app/src/main/kotlin/io/github/hosseinkarami_dev/near/rpc/client_app/MainActivity.kt
@@ -2,11 +2,22 @@ package io.github.hosseinkarami_dev.near.rpc.client_app
 
 import android.os.Bundle
 import androidx.core.app.ComponentActivity
+import androidx.lifecycle.lifecycleScope
 import io.github.hosseinkarami_dev.near.rpc.client.NearClient
+import io.github.hosseinkarami_dev.near.rpc.client.RpcResponse
+import io.github.hosseinkarami_dev.near.rpc.client.Utils.getResultOrNull
+import io.github.hosseinkarami_dev.near.rpc.models.BlockId
+import io.github.hosseinkarami_dev.near.rpc.models.RpcBlockRequest
+import io.github.hosseinkarami_dev.near.rpc.models.RpcBlockResponse
+import io.github.hosseinkarami_dev.near.rpc.models.RpcStatusResponse
+import io.github.hosseinkarami_dev.near.rpc.models.RpcTransactionStatusRequest
+import io.github.hosseinkarami_dev.near.rpc.models.SignedTransaction
+import io.github.hosseinkarami_dev.near.rpc.models.TxExecutionStatus
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +33,22 @@ class MainActivity : ComponentActivity() {
             httpClient = httpClient,
             baseUrl = "https://rpc.mainnet.near.org" // or testnet: https://rpc.testnet.near.org
         )
+
+            lifecycleScope.launch {
+                val response = nearClient.status()
+
+                when (response) {
+                    is RpcResponse.Failure -> {
+                        println("Error: ${response.error}")
+                    }
+
+                    is RpcResponse.Success -> {
+                        val result = response.getResultOrNull<RpcStatusResponse>()
+                        println("Result: $result")
+
+                    }
+                }
+
 //
 //        lifecycleScope.launch {
 //            val response = nearClient.tx(
@@ -44,5 +71,6 @@ class MainActivity : ComponentActivity() {
 //                }
 //            }
 //        }
+        }
     }
 }
