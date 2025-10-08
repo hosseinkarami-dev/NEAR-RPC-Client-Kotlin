473044220e5a5bc43e6ec34a33a158f6c77c2bed
Hossein Karami <49790581+hosseinkaramii@users.noreply.github.com>
2025-10-09 02:28:08 +0330

    fixed some bugs

--- app/src/main/kotlin/io/github/hosseinkarami_dev/near/rpc/client_app/MainActivity.kt
+++ app/src/main/kotlin/io/github/hosseinkarami_dev/near/rpc/client_app/MainActivity.kt
@@ -2,50 +2,9 @@ package io.github.hosseinkarami_dev.near.rpc.client_app
 
 import android.os.Bundle
 import androidx.core.app.ComponentActivity
-import androidx.lifecycle.lifecycleScope
-import io.github.hosseinkarami_dev.near.rpc.client.NearClient
-import io.github.hosseinkarami_dev.near.rpc.client.RpcResponse
-import io.github.hosseinkarami_dev.near.rpc.client.Utils.getResultOrNull
-import io.github.hosseinkarami_dev.near.rpc.models.BlockId
-import io.github.hosseinkarami_dev.near.rpc.models.RpcBlockRequest
-import io.github.hosseinkarami_dev.near.rpc.models.RpcBlockResponse
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val httpClient = HttpClient(CIO) {
-            install(ContentNegotiation) {
-                json()
-            }
-        }
-
-        val nearClient = NearClient(
-            httpClient = httpClient,
-            baseUrl = "https://rpc.mainnet.near.org" // or testnet: https://rpc.testnet.near.org
-        )
-
-            lifecycleScope.launch {
-                val response = nearClient.block(
-                    RpcBlockRequest.BlockId(BlockId.BlockHeight(167440515.toULong()))
-                )
-
-                when (response) {
-                    is RpcResponse.Failure -> {
-                        println("Error: ${response.error}")
-                    }
-
-                    is RpcResponse.Success -> {
-                        val result = response.getResultOrNull<RpcBlockResponse>()
-                        println("Result: $result")
-
-                    }
-            }
-        }
     }
 }
