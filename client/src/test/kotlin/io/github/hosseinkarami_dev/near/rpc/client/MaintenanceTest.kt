ce3fc5468cf4f97f3452b4d5aa331f2ff479d2ae
Hossein Karami <49790581+hosseinkaramii@users.noreply.github.com>
2025-10-10 18:14:42 +0330

    Test Coverage Added

---
+++ client/src/test/kotlin/io/github/hosseinkarami_dev/near/rpc/client/MaintenanceTest.kt
@@ -0,0 +1,56 @@
+package io.github.hosseinkarami_dev.near.rpc.client
+
+import io.github.hosseinkarami_dev.near.rpc.client.Utils.getResultOrNull
+import io.github.hosseinkarami_dev.near.rpc.models.AccountId
+import io.github.hosseinkarami_dev.near.rpc.models.BlockId
+import io.github.hosseinkarami_dev.near.rpc.models.CryptoHash
+import io.github.hosseinkarami_dev.near.rpc.models.Finality
+import io.github.hosseinkarami_dev.near.rpc.models.RangeOfUint64
+import io.github.hosseinkarami_dev.near.rpc.models.RpcBlockRequest
+import io.github.hosseinkarami_dev.near.rpc.models.RpcBlockResponse
+import io.github.hosseinkarami_dev.near.rpc.models.RpcError
+import io.github.hosseinkarami_dev.near.rpc.models.RpcMaintenanceWindowsRequest
+import io.github.hosseinkarami_dev.near.rpc.models.RpcStatusResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+class MaintenanceTest {
+
+    private lateinit var httpClient: HttpClient
+    private lateinit var nearClient: NearClient
+
+    @BeforeTest
+    fun setup() {
+        httpClient = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        nearClient = NearClient(
+            httpClient = httpClient,
+            baseUrl = "https://rpc.mainnet.near.org"
+        )
+    }
+
+    @AfterTest
+    fun teardown() {
+        httpClient.close()
+    }
+
+    @Test
+    fun testStatus() = runTest {
+        val response = nearClient.maintenanceWindows(RpcMaintenanceWindowsRequest(accountId = AccountId("neardome2340.near")))
+        val result = response.getResultOrNull<List<RangeOfUint64>>()
+        println("Maintenance Response: $result")
+        assertTrue { response is RpcResponse.Failure }
+    }
+}
