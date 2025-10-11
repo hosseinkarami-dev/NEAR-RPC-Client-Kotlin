126a81f1dcbde198200f130ad5726d01b7baac28
Hossein Karami <49790581+hosseinkaramii@users.noreply.github.com>
2025-10-11 12:15:47 +0330

    Test Coverage Added

---
+++ client/src/test/kotlin/io/github/hosseinkarami_dev/near/rpc/client/QueryTest2.kt
@@ -0,0 +1,56 @@
+package io.github.hosseinkarami_dev.near.rpc.client
+
+import io.github.hosseinkarami_dev.near.rpc.client.Utils.getResultOrNull
+import io.github.hosseinkarami_dev.near.rpc.models.AccountId
+import io.github.hosseinkarami_dev.near.rpc.models.BlockId
+import io.github.hosseinkarami_dev.near.rpc.models.RpcQueryRequest
+import io.github.hosseinkarami_dev.near.rpc.models.RpcQueryResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.assertTrue
+
+class QueryTest2 {
+
+    private lateinit var httpClient: HttpClient
+    private lateinit var nearClient: NearClient
+
+    @BeforeTest
+    fun setup() {
+        httpClient = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        nearClient = NearClient(
+            httpClient = httpClient,
+            baseUrl = "https://rpc.mainnet.near.org"
+        )
+    }
+
+    @AfterTest
+    fun teardown() {
+        httpClient.close()
+    }
+
+    @Test
+    fun testStatus() = runTest {
+        val response = nearClient.query(
+            RpcQueryRequest.ViewAccountByBlockId(
+                accountId = AccountId("relay.tg"),
+                blockId = BlockId.BlockHeight(167692398U),
+                requestType = RpcQueryRequest.ViewAccountByBlockId.RequestType.VIEW_ACCOUNT
+            )
+        )
+
+        val result = response.getResultOrNull<RpcQueryResponse>()
+        println("ViewAccountByBlockId Response: $result")
+        assertTrue { response is RpcResponse.Success }
+    }
+}
